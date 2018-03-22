efd070664c727e4d8c7dabe076b01c6d99cc7b97
nidatta01 <nidatta01@P2P523EC5.admin.bvsd.k12.co.us>
2018-03-22 15:05:44 -0600

    Fancy Broken Controller

--- src/Jinput/ControllerTestRunner.java
+++ src/Jinput/ControllerTestRunner.java
@@ -23,7 +23,7 @@ public class ControllerTestRunner {
 	private int yInc = 0;
 	public static float updatePos = 1;
 	// Time stuff
-	private int timeSpeed = 25;
+	private int timeSpeed = 250;
 	private TimeUnit timeUnit = TimeUnit.MILLISECONDS;
 
 	// The board object
@@ -48,27 +48,35 @@ public class ControllerTestRunner {
 				System.exit(0);
 			}
 			for (int i = 0; i < controllers.length; i++) {
-				controllers[i].poll();//unsure of what this does
-				EventQueue queue = controllers[i].getEventQueue();//the things that are happening on the controller
-				Event event = new Event();//Create and event. This means that we can use this later to make things move ????
-				while (queue.getNextEvent(event) && updatePos != 0) {//while there are events in the controller, and updatePos is not null..
+				controllers[i].poll();// unsure of what this does
+				EventQueue queue = controllers[i].getEventQueue();// the things that are happening on the controller
+				Event event = new Event();// Create and event. This means that we can use this later to make things move
+											// ????
+				while (queue.getNextEvent(event) && updatePos != 0) {// while there are events in the controller, and
+																		// updatePos is not null..
 					// Erase ball
 					board.colorRect(ballLocation.y, ballLocation.x, ballSize, ballSize, Color.BLACK);
 
 					// Update ball location
 
-					updatePos = event.getValue();//when buttons are being pushed, this is not zero (!= 0)
+					updatePos = event.getValue();// when buttons are being pushed, this is not zero (!= 0)
 					Component comp = event.getComponent();// every button is a component
 					System.out.println(comp.getName());
-					if (comp.getName().equals("X Rotation")) {//if the thing that updated "updatePos" to a nonzero is "X Rotation"
-							if(updatePos > 0) xInc = 1;
-							if(updatePos < 0) xInc = -1;
+					if (comp.getName().equals("X Rotation")) {// if the thing that updated "updatePos" to a nonzero is
+																// "X Rotation"
+						if (updatePos > 0)
+							xInc = 1;
+						if (updatePos < 0)
+							xInc = -1;
 						System.out.println("X Axis Dominant. xInc: " + xInc + " yInc: " + yInc);
 						ballLocation = new Rectangle(ballLocation.x + xInc, ballLocation.y + yInc, ballLocation.width,
 								ballLocation.height);
 					} else if (comp.getName().equals("Y Rotation")) {
-							yInc = (int) updatePos;
-	
+						if (updatePos > 0)
+							yInc = 1;
+						if (updatePos < 0)
+							yInc = -1;
+
 						System.out.println("Y Axis Dominant. xInc: " + xInc + " yInc: " + yInc);
 						ballLocation = new Rectangle(ballLocation.x + xInc, ballLocation.y + yInc, ballLocation.width,
 								ballLocation.height);
