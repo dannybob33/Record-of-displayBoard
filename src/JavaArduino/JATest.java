e0192f0529e191bf65aa7a69d5ac94b2560f678d
robert.hettmansperge <robert.hettmansperge@P2P523F65.admin.bvsd.k12.co.us>
2018-03-22 16:44:09 -0600

    Added JavaArduino Lib (easier to use than RxTx)

---
+++ src/JavaArduino/JATest.java
@@ -0,0 +1,29 @@
+package JavaArduino;
+
+import java.util.Scanner;
+
+import arduino.*;
+
+public class JATest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Arduino a = new Arduino ("COM5", 9600);
+		a.openConnection();
+		
+		Scanner s = new Scanner (System.in);
+		System.out.println("Enter string to send to the Arduino - q to quit");
+		String str="";
+		while (str.equals("q") == false) {
+			str = s.next();
+			a.serialWrite(str + "*", str.length()+1, 0);
+			System.out.println("Sent: " + str);
+			String r = a.serialRead();
+			System.out.println("Received: " + r);				
+		}		
+		a.closeConnection();
+		s.close();
+	}
+
+}
