c6630d4e6733a9e222da5021e88ee407296a6033
jmbass01 <jmbass01@P2P4DF366.admin.bvsd.k12.co.us>
2018-03-23 13:41:29 -0600

    Added commenting to navigator app

--- src/DisplayBoardEmulation/nativeApp/simpleNavRunner.java
+++ src/DisplayBoardEmulation/nativeApp/simpleNavRunner.java
@@ -5,10 +5,14 @@ import DisplayBoardEmulation.tron.TronApp;
 
 public class simpleNavRunner {
 	public static void main(String[] args) {
+		//Make Manager
 		ApplicationManager manager = new ApplicationManager();
+		//Add navigator
 		manager.addNavigatingApplication(new simpleNavApp());
+		//Add other apps
 		manager.addApplication(new TronApp());
 		manager.addApplication(new DiscountPongApp());
+		//Start program
 		manager.initialize();
 	}
 }
