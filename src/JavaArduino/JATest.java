b0161cb920d89e03fcfd456ef98859d9131e80ce
Robert <Robert@192.168.0.11>
2018-04-05 17:46:23 -0600

    Added wait for Arduino Reset

--- src/JavaArduino/JATest.java
+++ src/JavaArduino/JATest.java
@@ -9,10 +9,32 @@ import arduino.*;
 public class JATest {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		// create connection to Arduino
 		Arduino a = new Arduino ("COM7", 9600);
 		a.openConnection();
+		
+		// sleep for 5 seconds to allow Arduino to restart
+		
+		System.out.println("Waiting for Arduino to reset...");
+		try {
+			Thread.sleep(5000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		// test loop-back
+		
+		System.out.println("Testing simple loop-back");
+		System.out.println("Sending: Testing");
+		a.serialWrite("Testing"+'\0');
+		String r = "";
+		while (r.equals("")) {
+			r = a.serialRead();
+		}
+		System.out.println("Received: " + r);				
+		
 		
 		Scanner s = new Scanner (System.in);
 		System.out.println("Enter string to send to the Arduino - q to quit");
@@ -21,9 +43,8 @@ public class JATest {
 			str = s.next();
 			a.serialWrite(str+'\0');
 			System.out.println("Sent: " + str);
-			String r = "";
+			r="";
 			while (r.equals("")) {
-				System.out.println("trying");
 				r = a.serialRead();
 			}
 			System.out.println("Received: " + r);				
