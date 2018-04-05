0f307148620696212ac34aac68ced9a9f79b54be
jmbass01 <jmbass01@P2P4DF366.admin.bvsd.k12.co.us>
2018-04-05 11:18:20 -0600

    Started making a pong app

---
+++ src/DisplayBoardEmulation/actualPong/PongApp.java
@@ -0,0 +1,28 @@
+package DisplayBoardEmulation.actualPong;
+
+import java.awt.Rectangle;
+
+import DisplayBoardEmulation.emulator.DisplayBoard;
+import DisplayBoardEmulation.nativeApp.Application;
+
+public class PongApp extends Application {
+	
+	
+	@Override
+	public void start(DisplayBoard board) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void terminate() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public String getName() {
+		return "Pong";
+	}
+
+}
