3e1026a9a3a2449e94ab6d5a9f2c79e9dd0f857d
lahsia01 <lahsia01@P2P5E62CF.admin.bvsd.k12.co.us>
2018-05-15 13:42:06 -0600

    Jedi for navRunner

--- src/GifThing/Jedi.java
+++ src/GifThing/Jedi.java
@@ -3,6 +3,10 @@ package GifThing;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.ScheduledFuture;
+import java.util.concurrent.TimeUnit;
 
 import javax.imageio.ImageIO;
 
@@ -12,41 +16,54 @@ import DisplayBoardEmulation.nativeApp.Application;
 public class Jedi extends Application {
 	private boolean isRunning = false;
 	private DisplayBoard board;
+	private int timeSpeed = 75;
+	private TimeUnit timeUnit = TimeUnit.MILLISECONDS;
+	private final ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(1);
+	private int currImg=0;
+
+	private ScheduledFuture<?> future;
 
 	@Override
 	public void start(DisplayBoard board) {
+		// Create and show board
 		this.board = board;
+		this.board.show();
 		isRunning = true;
-		while (true) {
-			for (int i = 0; i <= 32; i++) {
-				String imagePath = "H:\\private\\CS2\\Memes\\vader.jpg";
-				if (i < 10) {
-					imagePath = "H:\\private\\CS2\\Memes\\JediGif\\frame_0" + i + "_delay-0.1s.gif";
-				} else {
-					imagePath = "H:\\private\\CS2\\Memes\\JediGif\\frame_" + i + "_delay-0.1s.gif";
-				}
-
-				File f = new File(imagePath);
-				BufferedImage img = null;
-				try {
-					img = ImageIO.read(f);
-				} catch (IOException e) {
-					// e.printStackTrace();
-				}
-				board.drawImage(img, 0, 0, board.COLS, board.ROWS);
-				board.repaintBoard();
-				wait(50);
-			}
-		}
+		// Setup timer
+		future = scheduler.scheduleAtFixedRate(update, timeSpeed, timeSpeed, timeUnit);
 	}
 
+	public final Runnable update = new Runnable() {
+		public void run() {
+					String imagePath = "H:\\private\\CS2\\Memes\\vader.jpg";
+					if (currImg < 10) {
+						imagePath = "H:\\private\\CS2\\Memes\\JediGif\\frame_0" + currImg + "_delay-0.1s.gif";
+					} else {
+						imagePath = "H:\\private\\CS2\\Memes\\JediGif\\frame_" + currImg + "_delay-0.1s.gif";
+					}
+					File f = new File(imagePath);
+					BufferedImage img = null;
+					try {
+						img = ImageIO.read(f);
+					} catch (IOException e) {
+						// e.printStackTrace();
+					}
+					board.drawImage(img, 0, 0, board.COLS, board.ROWS);
+					board.repaintBoard();
+					currImg++;
+					if(currImg>32) {
+						currImg=0;
+					}
+				}
+	};
+
 	public void wait(int milli) {
-		if(isRunning) {
-		try {
-			Thread.sleep(milli);
-		} catch (InterruptedException ex) {
-			Thread.currentThread().interrupt();
-		}
+		if (isRunning) {
+			try {
+				Thread.sleep(milli);
+			} catch (InterruptedException ex) {
+				Thread.currentThread().interrupt();
+			}
 		}
 	}
 
