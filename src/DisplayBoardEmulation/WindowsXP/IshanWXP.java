7e93277cdff0028d27c0427c1e874a63de2d0ac6
ikumar01 <ikumar01@P2P4DF2D6.admin.bvsd.k12.co.us>
2018-05-15 12:36:21 -0600

    [WindowsXP] Update. Version: V0.3.1 ~Ishan

--- src/DisplayBoardEmulation/WindowsXP/IshanWXP.java
+++ src/DisplayBoardEmulation/WindowsXP/IshanWXP.java
@@ -1,5 +1,8 @@
 package DisplayBoardEmulation.WindowsXP;
 
 public class IshanWXP {
-	
+	private void Desktop() {
+		drawImage(ImageIO.read(new File), 0, 0, DisplayBoard.ROWS, DisplayBoard.COLS);
+		action();
+	}
 }
