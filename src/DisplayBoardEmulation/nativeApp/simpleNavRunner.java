6a7ed23cc1acab2726b112e3c6a3148c2f9e65fe
tdodowd01 <tdodowd01@P2P5DE337.admin.bvsd.k12.co.us>
2018-05-15 14:13:14 -0600

    UPDATES

--- src/DisplayBoardEmulation/nativeApp/simpleNavRunner.java
+++ src/DisplayBoardEmulation/nativeApp/simpleNavRunner.java
@@ -26,7 +26,7 @@ public class simpleNavRunner {
 		manager.addApplication(new SortingApp());
 		manager.addApplication(new WebCamPhotoApp());
 		manager.addApplication(new SnakeApp());
-		manager.addApplication(new Jedi());
+		//manager.addApplication(new Jedi());
 		manager.addApplication(new fusionFeudApp());
 		manager.addApplication(new weatherTest());
 		manager.addApplication(new WinXP());
